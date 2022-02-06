813eb5d3c4fc3aa40baa2363803f8836a7191c4a
Ivan <taim-off@mail.ru>
2022-02-06 19:58:10 +0300

    homework 3 done

--- ObjectsAndMethods/src/Basket.java
+++ ObjectsAndMethods/src/Basket.java
@@ -4,11 +4,13 @@ public class Basket {
     private String items = "";
     private int totalPrice = 0;
     private int limit;
+    private double totalWeight = 0.0;
 
     public Basket() {
         increaseCount(1);
         items = "Список товаров:";
         this.limit = 1000000;
+
     }
 
     public Basket(int limit) {
@@ -23,18 +25,22 @@ public class Basket {
     }
 
     public static int getCount() {
+
         return count;
     }
-
     public static void increaseCount(int count) {
+
         Basket.count = Basket.count + count;
     }
-
     public void add(String name, int price) {
-        add(name, price, 1);
-    }
 
+        add(name, price, 1,0.0);
+    }
     public void add(String name, int price, int count) {
+        add(name, price,count,0.0);
+
+    }
+    public void add(String name, int price, int count, double weight) {
         boolean error = false;
         if (contains(name)) {
             error = true;
@@ -50,20 +56,30 @@ public class Basket {
         }
 
         items = items + "\n" + name + " - " +
-            count + " шт. - " + price;
+                count + " шт. - " + price + "руб. - " + weight + "кг";
+
         totalPrice = totalPrice + count * price;
+        totalWeight = totalWeight + weight * count;
+
     }
 
     public void clear() {
         items = "";
         totalPrice = 0;
+        totalWeight = 0;
     }
 
     public int getTotalPrice() {
+
         return totalPrice;
     }
+    public double getTotalWeight(){
+        return totalWeight;
+    }
+
 
     public boolean contains(String name) {
+
         return items.contains(name);
     }
 
@@ -73,6 +89,9 @@ public class Basket {
             System.out.println("Корзина пуста");
         } else {
             System.out.println(items);
+
         }
+        System.out.println("\nОбщая стоимость товаров: " + totalPrice +
+                "\nОбщий вес товаров: " + totalWeight);
     }
 }
--- ObjectsAndMethods/src/Main.java
+++ ObjectsAndMethods/src/Main.java
@@ -2,7 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         Basket basket = new Basket();
-        basket.add("Milk", 40);
+        basket.add("Молоко", 40, 2);
+        basket.add("Coffee", 400, 1,0.5);
+        basket.add("Лопата",300,2,2);
         basket.print("Milk");
+
+
+
+
     }
 }
