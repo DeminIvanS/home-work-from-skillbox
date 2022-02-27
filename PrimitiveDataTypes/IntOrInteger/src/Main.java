65ba0b78e9197ba3ed8210cc86c3d96d63577e57
Ivan <taim-off@mail.ru>
2022-02-27 19:47:57 +0300

    Задание по примитивам готово

--- PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -7,6 +7,17 @@ public class Main {
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+        for (int i = 0; i < 65536; i++) {
+            char c = (char) i;
+            if(i >= 'А' && i <= 'я') {
+                System.out.println(i + " - " + c);
+            }
+        }
+        char c1 = 'ё';
+        char c2 = 'Ё';
+        int charCode = c1;
+        int charCode2= c2;
 
-    }
-}
+        System.out.println(charCode + " - " + c1);
+        System.out.println(charCode2 + " - " + c2);
+}}
