d8a6231ea6316da5d0f791f3787130a556e9bd7a
Ivan <taim-off@mail.ru>
2022-11-28 19:40:01 +0300

    SQLAndHibernate среднее число продаж в месяц. с учетом замечаний

--- SQLAndHibernate/src/Main.java
+++ SQLAndHibernate/src/Main.java
@@ -1,12 +1,7 @@
 import java.sql.*;
-import java.time.LocalDateTime;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String key = null;
-        Map coursesAndDateSubscribs = new TreeMap();
-        List dateSubscribs;
 
         String url = "jdbc:mysql://localhost:3306/skillbox_sql_db";
         String user = "root";
@@ -14,55 +9,26 @@ public class Main {
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT pl.course_name, " +
-                    "pl.subscription_date  " +
-                    "FROM purchaseList pl " +
-                    "ORDER BY pl.course_name, pl.subscription_date;" );
-            dateSubscribs = new ArrayList<>();
-            while (resultSet.next()){
+            ResultSet resultSet = statement.executeQuery("SELECT DISTINCT p.course_name,\n" +
+                    "(MAX(DATE_FORMAT(p.subscription_date, '%m')) - MIN(DATE_FORMAT(p.subscription_date, '%m')))`months_sale`,\n" +
+                    "DATE_FORMAT(skillbox_sql_db.p.subscription_date,\n" +
+                    "'%m') `month`, count(*) `count`\n" +
+                    "FROM purchaselist p\n" +
+                    "GROUP BY p.course_name\n" +
+                    "ORDER BY subscription_date;");
+
+            while (resultSet.next()) {
 
                 String courseName = resultSet.getString("course_name");
-                String subscriptionDate = resultSet.getString("subscription_date");
-                if(key == null) {
-                    key = courseName;
-                }
-                    if(courseName.equals(key)){
-                        dateSubscribs.add(subscriptionDate);
-                    }else{
-                        coursesAndDateSubscribs.put(key, dateSubscribs);
-                        dateSubscribs = new ArrayList<>();
-                        key = courseName;
-                        dateSubscribs.add(subscriptionDate);
-                    }
+                double monthsSale = Double.parseDouble((resultSet.getString("months_sale")));
+                double countSale = Double.parseDouble((resultSet.getString("count")));
+                double averageSales = ((double) Math.round((countSale / monthsSale) * 100)) / 100;
+                System.out.println("Для курса: \"" + courseName + "\" - среднее количество покупок в месяц за 2018 год равно: " + averageSales);
             }
-            coursesAndDateSubscribs.put(key,dateSubscribs);
 
-            resultSet.close();
-            statement.close();
-            connection.close();
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-        for(Object courses : coursesAndDateSubscribs.keySet()) {
-             ArrayList dates = (ArrayList) coursesAndDateSubscribs.get(courses);
-             String beginDate = (String) dates.get(0);
-             String endDate = (String) dates.get(dates.size() - 1);
 
-
-
-            int delta = getMonthsSales(endDate) - getMonthsSales(beginDate);
-            double averageSales =  ((double) Math.round(((double)dates.size() / delta)*100))/100;
-            System.out.println("Для курса: \"" + courses + "\" - среднее количество покупок в месяц за 2018 год равно: " + averageSales );
-        }
-    }
-
-    public static int getMonthsSales(String stringDate) {
-
-        String toDate = stringDate.replace(" ", "T");
-        LocalDateTime date = LocalDateTime.parse(toDate);
-        int month = (date.getMonthValue());
-
-        return month;
     }
 }
